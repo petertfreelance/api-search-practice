61af4bc6ed4e12e7193b26c771f40c4fef37d6e7
Peter <petertfreelance@gmail.com>
2024-10-14 11:26:37 -0700

    add sort filter

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@
 //https://api.themoviedb.org/3/account/719ef349785fecd706a9b04f07619433/favorite/movie 
 
 // 'https://api.themoviedb.org/3/movie/changes
+
+
 async function renderMovies(filter=null) {
 
     const moviewrapper = document.getElementById('shop');
@@ -19,13 +21,10 @@ async function renderMovies(filter=null) {
       .then(response => console.log(''))
       .catch(err => console.error(err)));
       
-      const imageBaseUrl = 'https://image.tmdb.org/t/p/w600_and_h900_bestv2'
+    const imageBaseUrl = 'https://image.tmdb.org/t/p/w600_and_h900_bestv2'
 
-      let movies = await fetch('https://api.themoviedb.org/3/account/719ef349785fecd706a9b04f07619433/favorite/movies?title=deadpool', options)
-    
-      
+    let movies = await fetch('https://api.themoviedb.org/3/account/719ef349785fecd706a9b04f07619433/favorite/movies?title=deadpool', options)
 
-    
     let movieData = await movies.json();
 
     
@@ -33,25 +32,44 @@ async function renderMovies(filter=null) {
         console.log(filter.target.value)
         filter = filter.target.value;
         filtered_array = [];
-        // console.log(movieData)
-        movieData.results.forEach(element => {
-            console.log(element.title.indexOf(filter))
-            console.log(element.title)
-            if(element.title.toLowerCase().includes(filter)) {
-                
-                filtered_array.push(element);
-            }
-        });
 
-        movieData.results = filtered_array;
+        
+        if(filter != 'RELASE_DATE_ASC' && filter != 'RELASE_DATE_DSC' && filter != 'ALPHABETICAL') {
+            
+            movieData.results.forEach(element => {
+
+                if(element.title.toLowerCase().includes(filter)) {
+                    
+                    filtered_array.push(element);
+                }
+            });
+    
+            movieData.results = filtered_array;
+        } else if (filter == 'RELASE_DATE_ASC') {
+            
+
+            console.log(movieData.results);
+
+            movieData.results.sort((a,b) => {return a.release_date.localeCompare(b.release_date)})
+
+
+        } else if (filter == 'RELASE_DATE_DSC') {
+            filtered_array = movieData.results;
+
+            filtered_array.sort((a,b) => {return b.release_date.localeCompare(a.release_date)})
+
+            // movieData.results = filtered_array
+        } else if (filter == 'ALPHABETICAL') {
+            filtered_array = movieData.results;
+
+            filtered_array.sort((a,b) => {return a.title.localeCompare(b.title)})
+
+            // movieData.results = filtered_array
+        }
+        
+        
 
     }
-
-    // console.log(movies);
-
-    // console.log(movieData.results);
-
-    
 
     const movieHtml = movieData.results.map( movie => {
 
@@ -82,6 +100,35 @@ async function renderMovies(filter=null) {
     moviewrapper.classList.remove("movies__loading");
       moviewrapper.innerHTML = movieHtml;
     
+}
+function sortMovies(filter) {
+    const moviewrapper = document.getElementById('shop');
+
+    movieData = getMovies();
+    
+    }
+
+async function getMovies() {
+        let options = {
+            method: 'GET',
+            headers: {
+              accept: 'application/json',
+              Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYTQwNzIwNTRkNDFhMTZlNWZkM2FhNzY2MTgyNjczNSIsIm5iZiI6MTcyODg2NDI3OC4xOTI3NTksInN1YiI6IjY3MGM1ZGQ1M2JiNDU1N2M2NjliZDY3MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ti66TBc3pP40Ou45YW8VfpgrzkOAUBIf1tw1UAo3g0c'
+            }
+          };
+    
+          let authentication =await (fetch('https://api.themoviedb.org/3/authentication', options)
+          .then(response => response.json())
+          .then(response => console.log(''))
+          .catch(err => console.error(err)));
+          
+        
+    
+        moviesinfo = await fetch('https://api.themoviedb.org/3/account/719ef349785fecd706a9b04f07619433/favorite/movies?', options)
+
+        movieDatainfo = await movies.json();
+
+        return movieDatainfo;
 }
 
 function showMore(id) {
