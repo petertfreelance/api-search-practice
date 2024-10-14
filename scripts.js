d7c045304dfab0daf1bd912d68853435b6cbe62f
Peter <petertfreelance@gmail.com>
2024-10-14 10:30:45 -0700

    initial commit of week 2 project for api javascript practice

---
+++ scripts.js
@@ -0,0 +1,104 @@
+
+//https://api.themoviedb.org/3/account/719ef349785fecd706a9b04f07619433/favorite/movie 
+
+// 'https://api.themoviedb.org/3/movie/changes
+async function renderMovies(filter=null) {
+
+    const moviewrapper = document.getElementById('shop');
+    
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYTQwNzIwNTRkNDFhMTZlNWZkM2FhNzY2MTgyNjczNSIsIm5iZiI6MTcyODg2NDI3OC4xOTI3NTksInN1YiI6IjY3MGM1ZGQ1M2JiNDU1N2M2NjliZDY3MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ti66TBc3pP40Ou45YW8VfpgrzkOAUBIf1tw1UAo3g0c'
+        }
+      };
+
+      const authentication =await (fetch('https://api.themoviedb.org/3/authentication', options)
+      .then(response => response.json())
+      .then(response => console.log(''))
+      .catch(err => console.error(err)));
+      
+      const imageBaseUrl = 'https://image.tmdb.org/t/p/w600_and_h900_bestv2'
+
+      let movies = await fetch('https://api.themoviedb.org/3/account/719ef349785fecd706a9b04f07619433/favorite/movies?title=deadpool', options)
+    
+      
+
+    
+    let movieData = await movies.json();
+
+    
+    if(filter != null) {
+        console.log(filter.target.value)
+        filter = filter.target.value;
+        filtered_array = [];
+        // console.log(movieData)
+        movieData.results.forEach(element => {
+            console.log(element.title.indexOf(filter))
+            console.log(element.title)
+            if(element.title.toLowerCase().includes(filter)) {
+                
+                filtered_array.push(element);
+            }
+        });
+
+        movieData.results = filtered_array;
+
+    }
+
+    // console.log(movies);
+
+    // console.log(movieData.results);
+
+    
+
+    const movieHtml = movieData.results.map( movie => {
+
+        let description = movie.overview;
+
+        let short_description = description.substring(0, 50)
+
+        let more__description = description.substring(51, description.length)
+
+        return `<div id="${movie.id}" class="movie">
+          <figure class="movie__img--wrapper">
+            <img class="movie__img" src="${imageBaseUrl + movie.poster_path}" alt="" />
+          </figure>
+        
+          <div class="movie__title">
+            ${movie.title}
+            </div>
+            <div class="movie__date">
+              ${movie.release_date}
+            </div>
+            <div class="movie__description">
+              <p class="overview">${short_description} <span class="more">${more__description}</span> <a href="javascript:showMore(${movie.id})" class="show-more">more...</a></p>
+            </div>
+
+        </div>
+      </div>`;
+    }).join("");
+    moviewrapper.classList.remove("movies__loading");
+      moviewrapper.innerHTML = movieHtml;
+    
+}
+
+function showMore(id) {
+    movie = document.getElementById(id);
+
+    description = movie.querySelector('.movie__description').querySelector('.overview');
+
+    if (description.classList.contains("active")) {
+        description.querySelector(".show-more").innerHTML = "more..."
+        description.classList.remove("active")
+
+    } else {
+        description.classList +=' active'
+        description.querySelector(".show-more").innerHTML = "less..."
+    }
+}
+
+setTimeout(() =>{
+    document.readyState(renderMovies())
+  }, 1000)
